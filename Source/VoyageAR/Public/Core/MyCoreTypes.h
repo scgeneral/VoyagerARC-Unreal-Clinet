e39db1f48c4fa4ba23917a3a2804e55183a37202
Яков Каретный <karetny_iakov_work@mail.ru>
2023-01-05 19:52:33 +0200

    new

--- Source/VoyageAR/Public/Core/MyCoreTypes.h
+++ Source/VoyageAR/Public/Core/MyCoreTypes.h
@@ -1,6 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "Engine/DataTable.h"
+#include "Materials/MaterialInstance.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
 #include "MyCoreTypes.generated.h"
 
 USTRUCT(BlueprintType)
@@ -21,6 +25,19 @@ public:
 		TSoftObjectPtr<UTexture2D> Icon;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		TSoftObjectPtr<UMaterialInstance> MaterialInstance;
+};
+
+USTRUCT(Blueprintable)
+struct FFurnitureData : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FName Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TSoftObjectPtr<UStaticMesh> Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FFurnitureMaterial FurnitureMaterial;
 };
 
 
